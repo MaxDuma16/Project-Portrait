c41fd86a7901c48d81ce4f427357b14bb3074510
Max <maxduma16@gmail.com>
2020-12-29 17:18:57 +0200

    add db, cards

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -1,18 +1,43 @@
-const showMoreStyles = (trigger, styles) => {
-    const cards = document.querySelectorAll(styles),
-          btn = document.querySelector(trigger);
+import {getResourse} from '../services/requests';
 
-    cards.forEach(card => {
-        card.classList.add('anamaed', 'fadeinUp');
+const showMoreStyles = (trigger, wrapper) => {
+    const btn = document.querySelector(trigger);
+  //const   cards = document.querySelectorAll(styles);
+
+    // cards.forEach(card => {
+    //     card.classList.add('anamaed', 'fadeinUp');
+    // });
+
+    // btn.addEventListener('click', () => {
+    //     cards.forEach(card => {
+    //         card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
+    //         card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+    //     });
+    //     // btn.style.display = 'none';
+    //     btn.remove();
+    // });
+
+    btn.addEventListener('click', function() {
+        getResourse('assets/db.json')
+        .then(res => createCards(res.styles))
+        .catch(error => console.log(error));
+        this.remove();
     });
 
-    btn.addEventListener('click', () => {
-        cards.forEach(card => {
-            card.classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
-            card.classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+    function createCards(response) {
+        console.log(response);
+        response.forEach(({src, title, link}) => {
+            let card = document.createElement('div');
+            card.classList.add('animated', 'fadeInUp','col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+            card.innerHTML = `
+            <div class=styles-block>
+                <img src=${src} alt="style">
+                <h4>${title}</h4>
+                <a href=${link}>Подробнее</a>
+             </div>
+            `;
+            document.querySelector(wrapper).appendChild(card);
         });
-        // btn.style.display = 'none';
-        btn.remove();
-    });
+    }
 };
 export default showMoreStyles;
